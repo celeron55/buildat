7eef5ef423d0b5b07b65203d76e57585a0ce45d9
Perttu Ahola <celeron55@gmail.com>
2014-09-17 11:43:46 +0300

    util/codestyle.sh: Set character limit -xC80

--- util/codestyle.sh
+++ util/codestyle.sh
@@ -15,8 +15,9 @@ echo "header_files: $header_files"
 echo "cpp_files: $cpp_files"
 
 # Fix all that astyle is capable of
-astyle -z2 -W3 -k3 -p -w -Y -K -N -t -m1 $header_files
-astyle -z2 -W3 -k3 -p -w -Y -K -t -m1 $cpp_files
+# Note: Astyle's character limit doesn't count tabs as multiple spaces.
+astyle -z2 -W3 -k3 -p -w -Y -K -N -t4 -m1 -xC80 $header_files
+astyle -z2 -W3 -k3 -p -w -Y -K -t4 -m1 -xC80 $cpp_files
 
 # Remove spaces before semicolons
 sed -i -e 's/ *;/;/g' $header_files $cpp_files
