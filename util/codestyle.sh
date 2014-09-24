c0826539c744de9d67c0b78df366e93e7efae353
Perttu Ahola <celeron55@gmail.com>
2014-09-24 14:51:50 +0300

    util/codestyle.sh: Add Vim modeline magic to everything

--- util/codestyle.sh
+++ util/codestyle.sh
@@ -5,8 +5,8 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 header_files="$script_dir"/../src/*/*.h
 cpp_files="$script_dir"/../src/*/*.cpp
 
-header_files+=" ""$script_dir"/../test/testmodules/*/*.h
-cpp_files+=" ""$script_dir"/../test/testmodules/*/*.cpp
+header_files+=" "$(find "$script_dir"/../test/testmodules -name '*.h')
+cpp_files+=" "$(find "$script_dir"/../test/testmodules -name '*.cpp')
 
 header_files+=" "$(find "$script_dir"/../builtin -name '*.h')
 cpp_files+=" "$(find "$script_dir"/../builtin -name '*.cpp')
@@ -14,8 +14,16 @@ cpp_files+=" "$(find "$script_dir"/../builtin -name '*.cpp')
 header_files+=" "$(find "$script_dir"/../3rdparty/c55lib -name '*.h')
 cpp_files+=" "$(find "$script_dir"/../3rdparty/c55lib -name '*.cpp')
 
+lua_files=""$(find "$script_dir"/../extensions -name '*.lua')
+lua_files+=" "$(find "$script_dir"/../client -name '*.lua')
+lua_files+=" "$(find "$script_dir"/../test/testmodules -name '*.lua')
+
+cmake_files="$script_dir"/../CMakeLists.txt
+
 echo "header_files: $header_files"
 echo "cpp_files: $cpp_files"
+echo "lua_files: $lua_files"
+echo "cmake_files: $cmake_files"
 
 # Fix all that astyle is capable of
 # Note: Astyle's character limit doesn't count tabs as multiple spaces.
@@ -44,8 +52,17 @@ sed -i -e 's/" + /"+/g' $header_files $cpp_files
 sed -i -e 's/" *+$/"+/g' $header_files $cpp_files
 sed -i -e 's/^\(\t\+\)   \+/\1\t\t/g' $header_files $cpp_files
 
+# Fix or add Vim modeline magic
+sed -i '/^\/\/ vim: set /d' $header_files $cpp_files
+for f in $header_files $cpp_files; do
+	echo '// vim: set noet ts=4 sw=4:' >> $f
+done
+sed -i '/^# vim: set /d' $lua_files $cmake_files
+for f in $lua_files $cmake_files; do
+	echo '# vim: set noet ts=4 sw=4:' >> $f
+done
+
 # Format CMake too
-cmake_files="$script_dir"/../CMakeLists.txt
 sed -i -e 's/\(^[ \t]*\)\([A-Za-z_]\+\)[ \t]*(/\1\L\2(/' $cmake_files
 sed -i -e 's/([\t ]\+\([A-Za-z_${)]\)/(\1/g' $cmake_files
 sed -i -e 's/\([A-Za-z_}(]\)[\t ]\+)/\1)/g' $cmake_files
