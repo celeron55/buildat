1f30a0cc33f88e37e60fdac8087f54c024a2a1b2
Perttu Ahola <celeron55@gmail.com>
2014-09-26 13:25:26 +0300

    extensions/__menu: Use UIStack

--- extensions/__menu/init.lua
+++ extensions/__menu/init.lua
@@ -3,62 +3,41 @@
 -- Copyright 2014 Perttu Ahola <celeron55@gmail.com>
 local log = buildat.Logger("extension/__menu")
 local magic = require("buildat/extension/urho3d")
+local uistack = require("buildat/extension/uistack")
 local dump = buildat.dump
 local M = {safe = nil}
 log:info("extension/__menu/init.lua: Loading")
 
-local ui_stack_name_i = 1  -- For generating a unique name for each stack
-local function UIStack(root)
-	local self = {}
-	self.root = root
-	self.stack_name = "ui_stack_"..ui_stack_name_i
-	ui_stack_name_i = ui_stack_name_i + 1
-	self.element_name_i = 1
-	self.stack = {}
-	function self:push()
-		if #self.stack >= 1 then
-			local top = self.stack[#self.stack]
-			top:SetVisible(false)
-		end
-		local element_name = self.stack_name.."_"..self.element_name_i
-		local element = self.root:CreateChild("UIElement")
-		element:SetName(element_name)
-		element:SetStyleAuto()
-    	element:SetLayout(LM_HORIZONTAL, 0, IntRect(0, 0, 0, 0))
-    	element:SetAlignment(HA_CENTER, VA_CENTER)
-		self.element_name_i = self.element_name_i + 1
-		table.insert(self.stack, element)
-		return element
-	end
-	function self:pop()
-		local top = table.remove(self.stack)
-		self.root:RemoveChild(top)
-		if #self.stack >= 1 then
-			local top = self.stack[#self.stack]
-			top:SetVisible(true)
-		end
-	end
-	return self
-end
-
-local ui_stack = UIStack(ui.root)
+local ui_stack = uistack.UIStack(ui.root)
 
 local function launch_connect_to_server()
+	local root = ui_stack:push()
+
+	local style = cache:GetResource("XMLFile", "__menu/res/boot_style.xml")
+	root.defaultStyle = style
+
+	local layout = root:CreateChild("Window")
+	layout:SetStyleAuto()
+    layout:SetName("Layout")
+    layout:SetLayout(LM_HORIZONTAL, 20, IntRect(0, 0, 0, 0))
+    layout:SetAlignment(HA_CENTER, VA_CENTER)
+
+	magic.SubscribeToEvent("KeyDown", function(event_type, event_data)
+		if root:HasRecursiveFocus() then
+			local key = event_data:GetInt("Key")
+			if key == KEY_ESC then
+				log:info("KEY_ESC pressed at connect_to_server level")
+				ui_stack:pop(root)
+			end
+		end
+	end)
 end
 
 function M.boot()
+	local root = ui_stack:push()
+
 	local style = cache:GetResource("XMLFile", "__menu/res/boot_style.xml")
-	ui.root.defaultStyle = style
-
-	local root = ui_stack:push()
-
-    local test_element = root:CreateChild("Sprite")
-	test_element:SetTexture(
-			cache:GetResource("Texture2D", "__menu/res/icon_network.png"))
-	test_element.color = Color(.2, .2, .2)
-	test_element:SetFixedSize(400, 400)
-
-	local root = ui_stack:push()
+	root.defaultStyle = style
 
 	local layout = root:CreateChild("Window")
 	layout:SetStyleAuto()
@@ -101,10 +80,12 @@ function M.boot()
 	end)
 
 	magic.SubscribeToEvent("KeyDown", function(event_type, event_data)
-		local key = event_data:GetInt("Key")
-		if key == KEY_ESC then
-			log:info("KEY_ESC pressed; quitting")
-			engine:Exit()
+		if root:HasRecursiveFocus() then
+			local key = event_data:GetInt("Key")
+			if key == KEY_ESC then
+				log:info("KEY_ESC pressed at top level; quitting")
+				engine:Exit()
+			end
 		end
 	end)
 end
