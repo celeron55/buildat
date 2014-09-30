e1b33c08c069b7eba386d3344ecfbf328413d3b8
Perttu Ahola <celeron55@gmail.com>
2014-09-30 14:26:52 +0300

    games/{minigame,test,uitest}: Remove useless bloat from __loaders

--- games/uitest/__loader/__loader.cpp
+++ games/uitest/__loader/__loader.cpp
@@ -13,7 +13,6 @@ namespace __loader {
 struct Module: public interface::Module
 {
 	interface::Server *m_server;
-	ss_ m_shutdown_reason;
 
 	Module(interface::Server *server):
 		interface::Module("__loader"),
@@ -25,23 +24,17 @@ struct Module: public interface::Module
 	~Module()
 	{
 		log_v(MODULE, "__loader destruct");
-		if(m_shutdown_reason != ""){
-			log_w(MODULE, "Shut down: %s", cs(m_shutdown_reason));
-		}
 	}
 
 	void init()
 	{
 		log_v(MODULE, "__loader init");
 		m_server->sub_event(this, Event::t("core:load_modules"));
-		m_server->sub_event(this, Event::t("core:module_modified"));
 	}
 
 	void event(const Event::Type &type, const Event::Private *p)
 	{
 		EVENT_VOIDN("core:load_modules", on_load_modules)
-		EVENT_TYPEN("core:module_modified", on_module_modified,
-				interface::ModuleModifiedEvent)
 	}
 
 	void on_load_modules()
@@ -52,22 +45,13 @@ struct Module: public interface::Module
 
 		bool ok = m_server->load_module(info);
 		if(!ok){
-			m_shutdown_reason = ss_()+"Error loading builtin/loader";
-			m_server->shutdown(1);
+			m_server->shutdown(1, "Error loading builtin/loader");
 			return;
 		}
 
 		loader::access(m_server, [&](loader::Interface * i){
 			i->activate();
 		});
-	}
-
-	void on_module_modified(const interface::ModuleModifiedEvent &event)
-	{
-		log_v(MODULE, "__loader::on_module_modified(): %s", cs(event.name));
-		if(event.name == "__loader")
-			return;
-		m_server->reload_module(event.name);
 	}
 };
 
