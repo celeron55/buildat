93215a3bc85020b111a504ff187878989fa609b9
Perttu Ahola <celeron55@gmail.com>
2014-10-05 20:38:07 +0300

    extensions/experimental: Remove unused code

--- extensions/experimental/init.lua
+++ extensions/experimental/init.lua
@@ -4,24 +4,7 @@
 local log = buildat.Logger("extension/experimental")
 local M = {safe = {}}
 
-local subs_tick = {}
-
-function __buildat_tick(dtime)
-	for _, cb in ipairs(subs_tick) do
-		cb(dtime)
-	end
-end
-
-function M.safe.sub_tick(cb)
-	table.insert(subs_tick, cb)
-end
-function M.safe.unsub_tick(cb)
-	for i=#subs_tick,1,-1 do
-		if subs_tick[i] == cb then
-			table.remove(subs_tick, i)
-		end
-	end
-end
+-- Empty
 
 return M
 -- vim: set noet ts=4 sw=4:
