a71a4c56eff54ba1dfeee955d8151d88ddfb11d6
Perttu Ahola <celeron55@gmail.com>
2014-10-20 17:15:28 +0300

    interface/os

---
+++ src/impl/os.cpp
@@ -0,0 +1,17 @@
+// http://www.apache.org/licenses/LICENSE-2.0
+// Copyright 2014 Perttu Ahola <celeron55@gmail.com>
+#include "interface/os.h"
+#include <sys/time.h>
+
+namespace interface {
+namespace os {
+
+int64_t get_timeofday_us()
+{
+	struct timeval tv;
+	gettimeofday(&tv, nullptr);
+	return (int64_t)tv.tv_sec * 1000000 + (int64_t)tv.tv_usec;
+}
+
+}}
+// vim: set noet ts=4 sw=4:
---
+++ src/interface/os.h
@@ -0,0 +1,14 @@
+// http://www.apache.org/licenses/LICENSE-2.0
+// Copyright 2014 Perttu Ahola <celeron55@gmail.com>
+#pragma once
+#include "core/types.h"
+
+namespace interface
+{
+	namespace os
+	{
+		int64_t get_timeofday_us();
+	}
+}
+
+// vim: set noet ts=4 sw=4:
